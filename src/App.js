cdb85d83c02ed8136a052aa351a4f3ce4763109b
Carmendlcp <carmendelacallepinto@hotmail.com>
2024-02-02 18:01:38 +0100

    lengujes

--- src/App.js
+++ src/App.js
@@ -8,15 +8,33 @@ import DetalleCasas from './Components/Detalle/DetalleCasas';
 import { Link } from 'react-router-dom';
 import Detalle from './Components/Detalle/Detalle';
 // import Paginator from './core/paginator/Paginator'
+import React, { useState } from 'react';
+import ComponenteEspanol from './Languages/ComponenteEspanol';
+import ComponenteIngles from './Languages/ComponenteIngles';
+import { I18nextProvider, useTranslation } from 'react-i18next';
+import i18n from './i18n';
 
 
 function App() {
 
+  const { t } = useTranslation();
+  const [language, setLanguage] = useState(i18n.language);
+
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+    setLanguage(lang);
+  };
+
   return (
 
     <>
-  
+   <I18nextProvider i18n={i18n}/>
     <Router>
+    <div>     
+            <button onClick={() => changeLanguage('es')}><img src="bandera_espanol.png" alt="Español" /></button>
+            <button onClick={() => changeLanguage('en')}><img src="bandera_ingles.png" alt="Inglés" /></button>
+          </div>
+          
         <Routes>
           <Route path='/personajes' element={<Personajes/>}></Route>
           <Route path='/casas' element={<Casas/>}></Route>
@@ -28,10 +46,10 @@ function App() {
         <nav>
             <ul className='lista'>
               <li>
-                <Link to="/personajes"><h3>PERSONAJES</h3></Link>
+                <Link to="/personajes"><h3>{t("PERSONAJES")}</h3></Link>
               </li>
               <li>
-                <Link to="/casas"><h3>CASAS</h3></Link>
+                <Link to="/casas"><h3> CASAS</h3></Link>
               </li>
               <li>
                 <Link to="/cronologia"><h3>CRONOLOGIA</h3></Link>
@@ -42,6 +60,9 @@ function App() {
             </ul>
           </nav>
       </Router>
+
+      {i18n.language === 'es' ? <ComponenteEspanol /> : <ComponenteIngles />}
+      <I18nextProvider/>
 
     </>
   );
